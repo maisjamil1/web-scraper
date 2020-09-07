fb921e240ed2533ea2376cf37119883d5e635912
maisjamil1 <maisjamil17118@gmail.com>
2020-09-07 17:26:36 +0300

    2

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Lab: Web Scraping
-## Estimate of time needed to complete:-
+## Estimate of time needed to complete:3 h
 ## Start time: 2:00 pm
-## Finish time:-
-## Actual time needed to complete: 
+## Finish time:5 pm
+## Actual time needed to complete: 3 h
 ## Link: https://github.com/maisjamil1/web-scraper/pull/1
 ## Notes:
 
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,9 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
+
 URL = "https://en.wikipedia.org/wiki/History_of_Mexico"
-page = requests.get(URL)
-# print(page.content)
 
-soup = BeautifulSoup(page.content, 'html.parser')# to convert the text to obj
 
-print(soup)
+# test = soup.find(title="Wikipedia:Citation needed")
+# print(test)
+
+
+# _________________________________________
+# Count function must be named get_citations_needed_count
+# get_citations_needed takes in a url and returns an integer
+def get_citations_needed_count(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results =soup.find_all('a', href="/wiki/Wikipedia:Citation_needed")
+    print(len(results))
+    return len(results)
+
+# _________________________________________
+# Report function must be named get_citations_needed_report
+# get_citations_needed_report takes in a url and returns a string
+# the string should be formatted with each citation needed on own line, in order found.
+def get_citations_needed_report(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results =soup.find_all('a', href="/wiki/Wikipedia:Citation_needed")
+    # print(results)
+
+    # print(results[0].text.strip())
+    li1=[]
+    # li2=[]
+    for i in range (len(results)):
+        x=i+1
+        print(f'citations number{x}')
+        print('-'*100)
+        print(results[i].parent.parent.parent.getText())
+        # li1.append(results[i].parent.parent.parent.getText())
+        li1.append(results[i].parent.parent.parent.getText().strip())
+        return li1
+
+
+    # for i in range(len(li1)):
+    #     y=li1[i].split('[')[0]
+    #     li2.append(y)
+    # print(f'list 2 --->{li2}')
+
+
+  
+
+
+
+
+
+if __name__ == "__main__":
+    get_citations_needed_count(URL)
+    get_citations_needed_report(URL)
+    # pass
